3d030c52491f7b3a9d2aee3d183b2609576fe747
Black-Sparkles <122908587+Black-Sparkles@users.noreply.github.com>
2025-04-01 13:41:43 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,31 +1,5 @@
-// async function submitVote(option) {
-//   await fetch("/vote", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ option }),
-//   });
-//   fetchResults();
-// }
-
-// async function fetchResults() {
-//   const response = await fetch("/results");
-//   const data = await response.json();
-//   const resultsList = document.getElementById("resultsList");
-//   resultsList.innerHTML = "";
-//   Object.keys(data).forEach((key) => {
-//     const li = document.createElement("li");
-//     li.textContent = `${key}: ${data[key]} votes`;
-//     resultsList.appendChild(li);
-//   });
-// }
-
-// // Fetch initial results every 2 seconds
-// setInterval(fetchResults, 2000);
-// fetchResults();
-
-
 async function submitVote(option) {
-  await fetch("http://localhost:3000/vote", {
+  await fetch("/vote", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ option }),
@@ -34,7 +8,7 @@ async function submitVote(option) {
 }
 
 async function fetchResults() {
-  const response = await fetch("http://localhost:3000/results");
+  const response = await fetch("/results");
   const data = await response.json();
   const resultsList = document.getElementById("resultsList");
   resultsList.innerHTML = "";
@@ -45,5 +19,31 @@ async function fetchResults() {
   });
 }
 
+// Fetch initial results every 2 seconds
 setInterval(fetchResults, 2000);
 fetchResults();
+
+
+// async function submitVote(option) {
+//   await fetch("http://localhost:3000/vote", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ option }),
+//   });
+//   fetchResults();
+// }
+
+// async function fetchResults() {
+//   const response = await fetch("http://localhost:3000/results");
+//   const data = await response.json();
+//   const resultsList = document.getElementById("resultsList");
+//   resultsList.innerHTML = "";
+//   Object.keys(data).forEach((key) => {
+//     const li = document.createElement("li");
+//     li.textContent = `${key}: ${data[key]} votes`;
+//     resultsList.appendChild(li);
+//   });
+// }
+
+// setInterval(fetchResults, 2000);
+// fetchResults();
